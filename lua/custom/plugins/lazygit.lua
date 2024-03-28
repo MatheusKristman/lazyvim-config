9d769b823fe51194e6411f07b4b679529f2cd96b
MatheusKristman <kristman058@gmail.com>
2024-03-28 17:50:05 -0300

    added lazygit

---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}
