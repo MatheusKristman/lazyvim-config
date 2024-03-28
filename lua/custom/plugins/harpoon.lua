0097d19a152834746b9ddb3e35c1a2ee57f6c8a8
MatheusKristman <kristman058@gmail.com>
2024-03-28 17:55:43 -0300

    added new descriptions to keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -16,23 +16,23 @@ return {
 
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():append()
-      end)
+      end, { desc = 'Harpoon [A]dd file' })
       vim.keymap.set('n', '<C-e>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
+      end, { desc = 'Harpoon open [E]xplorer' })
 
       vim.keymap.set('n', '<leader>1', function()
         harpoon:list():select(1)
-      end)
+      end, { desc = 'Harpoon go to file [1]' })
       vim.keymap.set('n', '<leader>2', function()
         harpoon:list():select(2)
-      end)
+      end, { desc = 'Harpoon go to file [2]' })
       vim.keymap.set('n', '<leader>3', function()
         harpoon:list():select(3)
-      end)
+      end, { desc = 'Harpoon go to file [3]' })
       vim.keymap.set('n', '<leader>4', function()
         harpoon:list():select(4)
-      end)
+      end, { desc = 'Harpoon go to file [4]' })
     end,
   },
 }
