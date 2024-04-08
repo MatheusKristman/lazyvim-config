3eaf9bad3afd837e54f7d039d0db4c7f440dd12f
MatheusKristman <kristman058@gmail.com>
2024-04-08 13:24:53 -0300

    added new comment plugin

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -36,4 +36,15 @@ return {
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  {
+    'echasnovski/mini.comment',
+    event = 'VeryLazy',
+    opts = {
+      options = {
+        custom_commentstring = function()
+          return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    },
+  },
 }
