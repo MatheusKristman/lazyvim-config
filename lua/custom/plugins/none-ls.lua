3bf0cccea114a80a43ce5416e5ac214ef08b92a1
MatheusKristman <kristman058@gmail.com>
2024-04-24 20:42:27 -0300

    updated tab width

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,48 +1,45 @@
 return {
-    'nvimtools/none-ls.nvim',
-    dependencies = {
-        'nvimtools/none-ls-extras.nvim',
-    },
-    config = function()
-        local null_ls = require 'null-ls'
-        local null_ls_utils = require 'null-ls.utils'
-        local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-
-        null_ls.setup {
-            root_dir = null_ls_utils.root_pattern('.null-ls-root', 'Makefile', '.git', 'package.json'),
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prismaFmt,
-                null_ls.builtins.formatting.prettierd.with {
-                    env = {
-                        PRETTIERD_DEFAULT_CONFIG = vim.fn.expand '~/.config/nvim/utils/linter-config/.prettierrc.json',
-                    },
-                    prefer_local = true,
-                },
-                require('none-ls.diagnostics.eslint').with {
-                    condition = function(utils)
-                        return utils.root_has_file { '.eslintrc.js', '.eslintrc.cjs' } -- only enable if root has .eslintrc.js or .eslintrc.cjs
-                    end,
-                },
-            },
-            -- AutoFormat on Save
-            on_attach = function(client, bufnr)
-                if client.supports_method 'textDocument/formatting' then
-                    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-                    vim.api.nvim_create_autocmd('BufWritePre', {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-                            -- vim.lsp.buf.formatting_sync()
-                            vim.lsp.buf.format { async = false }
-                        end,
-                    })
-                end
-            end,
-        }
-
-        vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, { desc = '[G]o [F]ormat' })
-    end,
+  --   'nvimtools/none-ls.nvim',
+  --   dependencies = {
+  --     'nvimtools/none-ls-extras.nvim',
+  --   },
+  --   config = function()
+  --     local null_ls = require 'null-ls'
+  --     local null_ls_utils = require 'null-ls.utils'
+  --     local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+  --
+  --     null_ls.setup {
+  --       root_dir = null_ls_utils.root_pattern('.null-ls-root', 'Makefile', '.git', 'package.json'),
+  --       sources = {
+  --         null_ls.builtins.formatting.stylua,
+  --         null_ls.builtins.formatting.prismaFmt,
+  --         null_ls.builtins.formatting.prettierd.with {
+  --           env = {
+  --             PRETTIERD_DEFAULT_CONFIG = vim.fn.expand '~/.config/nvim/utils/linter-config/.prettierrc.json',
+  --           },
+  --           prefer_local = true,
+  --         },
+  --         require('none-ls.diagnostics.eslint').with {
+  --           condition = function(utils)
+  --             return utils.root_has_file { '.eslintrc.js', '.eslintrc.cjs' } -- only enable if root has .eslintrc.js or .eslintrc.cjs
+  --           end,
+  --         },
+  --       },
+  --       -- AutoFormat on Save
+  --       on_attach = function(client, bufnr)
+  --         if client.supports_method 'textDocument/formatting' then
+  --           vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+  --           vim.api.nvim_create_autocmd('BufWritePre', {
+  --             group = augroup,
+  --             buffer = bufnr,
+  --             callback = function()
+  --               vim.lsp.buf.format { async = false }
+  --             end,
+  --           })
+  --         end
+  --       end,
+  --     }
+  --
+  --     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, { desc = '[G]o [F]ormat' })
+  --   end,
 }
