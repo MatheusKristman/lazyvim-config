e055bd71a968238431c1cb9995a81a03a6c21a76
MatheusKristman <kristman058@gmail.com>
2024-04-24 11:53:32 -0300

    updated lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -15,7 +15,7 @@ return {
             options = {
                 globalstatus = true,
                 component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
             },
             sections = {
                 lualine_a = {
