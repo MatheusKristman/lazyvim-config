9a689f4f24b56b2934f134d36701fafeace39bdc
MatheusKristman <kristman058@gmail.com>
2024-05-25 12:12:24 -0300

    added neorg

---
+++ lua/custom/plugins/neorg.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'vhyrro/luarocks.nvim',
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
+    'nvim-neorg/neorg',
+    dependencies = { 'luarocks.nvim' },
+    lazy = true,
+    version = '*',
+    -- put any other flags you wanted to pass to lazy here!
+    config = true,
+  },
+}
