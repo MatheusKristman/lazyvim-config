c1872cf244bd4b09b173acd8b1181ce22daa6b89
MatheusKristman <kristman058@gmail.com>
2024-09-09 23:39:32 -0300

    added new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,9 +12,15 @@ return {
     priority = 1000,
   },
   {
+    "sainnhe/gruvbox-material",
+    lazy = true,
+    name = "gruvbox-material",
+    priority = 1000,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "gruvbox-material",
     },
   },
 }
