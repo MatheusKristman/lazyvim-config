9639455cd2ec68207e5d19f160a233276e73d830
MatheusKristman <kristman058@gmail.com>
2025-03-12 18:43:58 -0300

    removed move line function

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -38,3 +38,6 @@ keymap.set("n", "<C-S-l>", "<C-w>-")
 keymap.set("n", "<C-j>", function()
   vim.diagnostic.goto_next()
 end, opts)
+
+keymap.del({ "i", "n", "v" }, "<A-j>")
+keymap.del({ "i", "n", "v" }, "<A-k>")
